70307aef76233361bc2d5184af7d4b0789056110
Kuvin77 <v.kustyshev@gmail.com>
2022-05-19 16:58:50 +0300

    Second commit

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -8,7 +8,7 @@ import ru.netology.entity.Location;
 public class GeoServiceImplTest {
 
     @Test
-    public void byIpTest() {
+    public void byIpTest1() {
         final Location target = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
         final String someIp = "172.0.32.11";
         GeoService geo = new GeoServiceImpl();
@@ -17,8 +17,28 @@ public class GeoServiceImplTest {
 
         Assertions.assertEquals(target.getStreet(), result.getStreet());
 
+    }
 
+    @Test
+    public void byIpTest2() {
+        final Location target = new Location("New York", Country.USA, " 10th Avenue", 32);
+        final String someIp = "172.0.32.11";
+        GeoService geo = new GeoServiceImpl();
 
+        final Location result = geo.byIp(someIp);
+
+        Assertions.assertNotEquals(target.getStreet(), result.getStreet());
+
+    }
+    @Test
+    public void byIpTest3() {
+        final Location target = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
+        final String someIp = "96.44.183.149";
+        GeoService geo = new GeoServiceImpl();
+
+        final Location result = geo.byIp(someIp);
+
+        Assertions.assertNotEquals(target.getStreet(), result.getStreet());
 
     }
 }
--- src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -13,4 +13,22 @@ public class LocalizationServiceImplTest {
         final String msg_checked = loc.locale(country);
         Assertions.assertEquals(msg, msg_checked);
     }
+
+    @Test
+    void locale2Test() {
+        String msg = "Welcome";
+        Country country = Country.USA;
+        LocalizationService loc = new LocalizationServiceImpl();
+        final String msg_checked = loc.locale(country);
+        Assertions.assertEquals(msg, msg_checked);
+    }
+
+    @Test
+    void locale3Test() {
+        String msg = "Welcome";
+        Country country = Country.RUSSIA;
+        LocalizationService loc = new LocalizationServiceImpl();
+        final String msg_checked = loc.locale(country);
+        Assertions.assertNotEquals(msg, msg_checked);
+    }
 }
